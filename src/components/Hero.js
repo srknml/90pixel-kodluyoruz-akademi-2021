9fe4b9aa106a6288ba6d9c7fd99c4093bb516221
Schude <nemli.serkan2@gmail.com>
2021-05-20 18:02:13 +0300

    Hero text  effect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import hero from '../assets/hero-image.jpg';
-import './styles/Hero.css'
+import React from "react";
+import hero from "../assets/hero-image.jpg";
+import "./styles/Hero.css";
 export default function Hero() {
+ 
+    const makeSpans = () => {
+        const [...elements] = document.querySelectorAll(".hero-title")
+        return elements.map(element => {
+          const text = element.innerText.split('')
+          const spans = text
+            .map(letter => '<span>' + letter + '</span>')
+            .join('')
+          return element.innerHTML = spans
+        })
+      }
+      makeSpans()
+
+
     return (
-        <div className="hero" >
-            <h1 className="hero-title" >90Pixel & Kodluyoruz Akademi React Katılımcıları </h1>
-            <h1 className ="hero-title" >  </h1>
-            <div className="hero-img-box" > <img src = {hero} alt="hero"/>  </div>
-            
+        <div className="hero">
+            <h1
+
+                className="hero-title"
+            >
+                90Pixel &amp; Kodluyoruz Akademi React Katılımcıları
+            </h1>
+            <div className="hero-img-box">
+                {" "}
+                <img src={hero} alt="hero" />{" "}
+            </div>
         </div>
-    )
+    );
 }
