ff7dc83cdc1b70e93bb33b7c069c03b5495c071c
Schude <nemli.serkan2@gmail.com>
2021-05-20 22:15:42 +0300

    create method checks profile variable

--- src/database/databaseMethods.js
+++ src/database/databaseMethods.js
@@ -4,37 +4,21 @@ export const dbMethods = {
     // firebase store helper methods go here...
 
     create: (profile) => {
-        console.log(profile);
-        db.collection("react")
-            .doc(profile.id)
-            .set(profile)
-            .then(() => {
-                console.log("Document successfully written!");
-            })
-            .catch((error) => {
-                console.error("Error writing document: ", error);
-            });
+        if (Object.keys(profile).length !== 0) {
+            db.collection("react")
+                .doc(profile.id)
+                .set(profile)
+                .then(() => {
+                    console.log(("Document successfully written!"))
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+        }
     },
 
     get: async () => {
         const snapshot = await db.collection("react").get();
-        return snapshot.docs.map((doc) => doc.data())
-        
+        return snapshot.docs.map((doc) => doc.data());
     },
-    remove: (uid, todoID) => {
-        db.collection(uid)
-            .doc(todoID)
-            .delete()
-            .then(() => {
-                console.log("Document successfully deleted!");
-            })
-            .catch((error) => {
-                console.error("Error removing document: ", error);
-            });
-    },
-    //     update: (uid, todo) => {
-    //         db.collection(uid).doc(todo.id).update({
-    //             finished: todo.finished,
-    //         });
-    //     },
 };
