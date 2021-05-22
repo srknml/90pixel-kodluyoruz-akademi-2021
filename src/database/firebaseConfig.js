c769fdfad9052937c44d90f2bb8659733bfe5d63
Schude <nemli.serkan2@gmail.com>
2021-05-23 00:31:31 +0300

    firebase auth and firebase/github auth methods added.

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,28 +1,26 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    measurementId: process.env.REACT_APP_measurementId,
+};
 
-  if (!firebase.apps.length) {
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
     firebase.initializeApp({});
-  } else {
+} else {
     firebase.app(); // if already initialized, use that one
-  }
-  export const db = firebase.firestore();
-  // eslint-disable-next-line import/no-anonymous-default-export
-  export default {
-    firebaseConfig,
-  };
+}
+export const auth = firebase.auth;
+export const db = firebase.firestore();
